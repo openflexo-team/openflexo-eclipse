1e932e237fc90c0cb1c77eb545adc52d2c36f598
Gilles Besançon <gilles.besancon@external.thalesgroup.com>
2013-02-20 10:28:08 +0100

    MEDIUM : Add URI in emf.properties file.

--- org.openflexo.emfconnector.metamodel.exporter/src/org/openflexo/emfconnector/metamodel/exporter/EMFMetaModelExportWizard.java
+++ org.openflexo.emfconnector.metamodel.exporter/src/org/openflexo/emfconnector/metamodel/exporter/EMFMetaModelExportWizard.java
@@ -66,56 +66,39 @@ public class EMFMetaModelExportWizard extends Wizard implements IExportWizard {
 	 */
 	@Override
 	public boolean performFinish() {
-		exportMetaModel(page.getMetaModelUri(), page.getMetaModelEPackage(),
-				page.getExportPath());
+		exportMetaModel(page.getMetaModelUri(), page.getMetaModelEPackage(), page.getExportPath());
 		return true;
 	}
 
-	protected void exportMetaModel(String metaModelUri,
-			EPackage metaModelPackage, String exportPath) {
+	protected void exportMetaModel(String metaModelUri, EPackage metaModelPackage, String exportPath) {
 		try {
-			URI genModelLocation = EcorePlugin.INSTANCE
-					.getEPackageNsURIToGenModelLocationMap().get(metaModelUri);
-			Resource.Factory genModelFactory = Resource.Factory.Registry.INSTANCE
-					.getFactory(genModelLocation);
-			Resource genModelResource = genModelFactory
-					.createResource(genModelLocation);
+			URI genModelLocation = EcorePlugin.INSTANCE.getEPackageNsURIToGenModelLocationMap().get(metaModelUri);
+			Resource.Factory genModelFactory = Resource.Factory.Registry.INSTANCE.getFactory(genModelLocation);
+			Resource genModelResource = genModelFactory.createResource(genModelLocation);
 			genModelResource.load(null);
 			EList<EObject> genModelContents = genModelResource.getContents();
-			if (genModelContents.size() == 1
-					&& genModelContents.get(0) instanceof GenModel) {
+			if (genModelContents.size() == 1 && genModelContents.get(0) instanceof GenModel) {
 				GenModel genModel = (GenModel) genModelContents.get(0);
 				EList<GenPackage> genPackages = genModel.getGenPackages();
 				if (genPackages.size() == 1) {
 					GenPackage genPackage = genPackages.get(0);
 					String fileExtension = genPackage.getFileExtension();
-					Object resourceFactoryObject = Resource.Factory.Registry.INSTANCE
-							.getExtensionToFactoryMap().get(fileExtension);
+					Object resourceFactoryObject = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().get(fileExtension);
 					if (resourceFactoryObject == null) {
-						resourceFactoryObject = Resource.Factory.Registry.INSTANCE
-								.getExtensionToFactoryMap().get("*");
+						resourceFactoryObject = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().get("*");
 					}
 					Resource.Factory resourceFactory = null;
 					if (resourceFactoryObject instanceof Resource.Factory.Descriptor) {
-						resourceFactory = ((Resource.Factory.Descriptor) resourceFactoryObject)
-								.createFactory();
+						resourceFactory = ((Resource.Factory.Descriptor) resourceFactoryObject).createFactory();
 					} else {
 						resourceFactory = (Resource.Factory) resourceFactoryObject;
 					}
 
-					Class<?>[] interfaces = metaModelPackage.getClass()
-							.getInterfaces();
-					if (interfaces.length == 1 && interfaces[0].isInterface()
-							&& EPackage.class.isAssignableFrom(interfaces[0])) {
-						exportModel(
-								exportPath,
-								FrameworkUtil.getBundle(
-										metaModelPackage.getClass())
-										.getSymbolicName(), FrameworkUtil
-										.getBundle(resourceFactory.getClass())
-										.getSymbolicName(), fileExtension,
-								interfaces[0].getCanonicalName(),
-								resourceFactory.getClass().getCanonicalName());
+					Class<?>[] interfaces = metaModelPackage.getClass().getInterfaces();
+					if (interfaces.length == 1 && interfaces[0].isInterface() && EPackage.class.isAssignableFrom(interfaces[0])) {
+						exportModel(exportPath, FrameworkUtil.getBundle(metaModelPackage.getClass()).getSymbolicName(), FrameworkUtil
+								.getBundle(resourceFactory.getClass()).getSymbolicName(), metaModelUri, fileExtension,
+								interfaces[0].getCanonicalName(), resourceFactory.getClass().getCanonicalName());
 					}
 				}
 			}
@@ -124,31 +107,26 @@ public class EMFMetaModelExportWizard extends Wizard implements IExportWizard {
 		}
 	}
 
-	protected void exportModel(String exportPath, String ePackageBundleName,
-			String resourceFactoryBundleName, String extension,
-			String ePackage, String resourceFactory) {
-		File bundleNameFile = new File(exportPath + File.separator
-				+ ePackageBundleName);
+	protected void exportModel(String exportPath, String ePackageBundleName, String resourceFactoryBundleName, String metaModelUri,
+			String extension, String ePackage, String resourceFactory) {
+		File bundleNameFile = new File(exportPath + File.separator + ePackageBundleName);
 		bundleNameFile.mkdirs();
 		Set<File> bundleFiles = new HashSet<File>();
 		Set<String> bundleIdToAvoid = new HashSet<String>();
-		exportBundle(ePackageBundleName, bundleNameFile.getAbsolutePath(),
-				bundleFiles, bundleIdToAvoid);
-		exportBundle(resourceFactoryBundleName,
-				bundleNameFile.getAbsolutePath(), bundleFiles, bundleIdToAvoid);
+		exportBundle(ePackageBundleName, bundleNameFile.getAbsolutePath(), bundleFiles, bundleIdToAvoid);
+		exportBundle(resourceFactoryBundleName, bundleNameFile.getAbsolutePath(), bundleFiles, bundleIdToAvoid);
 		for (File bundleFile : bundleFiles) {
-			File destFile = new File(bundleNameFile.getAbsoluteFile()
-					+ File.separator + bundleFile.getName());
+			File destFile = new File(bundleNameFile.getAbsoluteFile() + File.separator + bundleFile.getName());
 			copyFile(bundleFile, destFile);
 		}
 		try {
 			Properties properties = new Properties();
+			properties.put("URI", metaModelUri);
 			properties.put("EXTENSION", extension);
 			properties.put("PACKAGE", ePackage);
 			properties.put("RESOURCE_FACTORY", resourceFactory);
 			FileOutputStream fos = new FileOutputStream(new File(
-					(exportPath + File.separator + ePackageBundleName
-							+ File.separator + "emf.properties")));
+					(exportPath + File.separator + ePackageBundleName + File.separator + "emf.properties")));
 			properties.store(fos, "");
 			fos.close();
 		} catch (FileNotFoundException e) {
@@ -158,22 +136,16 @@ public class EMFMetaModelExportWizard extends Wizard implements IExportWizard {
 		}
 	}
 
-	protected void exportBundle(String bundleSymbolicName, String exportPath,
-			Set<File> bundleFiles, Set<String> bundleToAvoid) {
+	protected void exportBundle(String bundleSymbolicName, String exportPath, Set<File> bundleFiles, Set<String> bundleToAvoid) {
 		try {
 			if (!bundleToAvoid.contains(bundleSymbolicName)) {
 				bundleToAvoid.add(bundleSymbolicName);
-				BundleDescription bundleDescription = Platform
-						.getPlatformAdmin().getState()
-						.getBundle(bundleSymbolicName, null);
-				Bundle bundle = Platform.getBundle(bundleDescription
-						.getSymbolicName());
+				BundleDescription bundleDescription = Platform.getPlatformAdmin().getState().getBundle(bundleSymbolicName, null);
+				Bundle bundle = Platform.getBundle(bundleDescription.getSymbolicName());
 				File bundleFile = FileLocator.getBundleFile(bundle);
 				bundleFiles.add(bundleFile);
-				for (BundleSpecification bundleRequirement : bundleDescription
-						.getRequiredBundles()) {
-					exportBundle(bundleRequirement.getName(), exportPath,
-							bundleFiles, bundleToAvoid);
+				for (BundleSpecification bundleRequirement : bundleDescription.getRequiredBundles()) {
+					exportBundle(bundleRequirement.getName(), exportPath, bundleFiles, bundleToAvoid);
 				}
 			}
 		} catch (IOException e) {
