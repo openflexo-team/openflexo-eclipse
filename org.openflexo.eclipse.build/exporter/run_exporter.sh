dbbb88800265bb148c9579a863e0fa0bd15f78a6
Gilles Besançon <gilles.besancon@gmail.com>
2013-11-20 20:39:41 -0600

    Add uninstall script Modify run script

--- org.openflexo.eclipse.build/exporter/run_exporter.sh
+++ org.openflexo.eclipse.build/exporter/run_exporter.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cd $1
 java -jar plugins/org.eclipse.equinox.launcher_*.jar\
 	-data workspace\
 	-application org.openflexo.emfconnector.metamodel.exporter.application.dialog.application
+cd ..
---
+++ org.openflexo.eclipse.build/exporter/uninstall_exporter.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+rm -rf director
+unzip *director*.zip
+cd director
+java -jar plugins/org.eclipse.equinox.launcher_*.jar\
+	-data workspace\
+	-application org.eclipse.equinox.p2.director\
+	-destination $1\
+	-profileProperties org.eclipse.update.install.features=true\
+	-uninstallIU org.openflexo.emfconnector.metamodel.exporter.updatesite.feature.feature.group
+cd ..
+rm -rf director
