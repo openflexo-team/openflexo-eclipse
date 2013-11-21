f48c4c5cd5c7ac4cba9f5a4977e2035ea9e9fa68
Gilles Besançon <gilles.besancon@gmail.com>
2013-11-20 19:29:32 -0600

    Create exporter packaging

---
+++ org.openflexo.eclipse.build/exporter/install_exporter.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+rm -rf director
+rm -rf updatesite-org.openflexo.emfconnector.metamodel.exporter
+unzip *director*.zip
+unzip *exporter*.zip -d updatesite-org.openflexo.emfconnector.metamodel.exporter
+cd director
+java -jar plugins/org.eclipse.equinox.launcher_*.jar\
+	-data workspace -application org.eclipse.equinox.p2.director\
+	-destination $1\
+	-repository file://`pwd`/../updatesite-org.openflexo.emfconnector.metamodel.exporter\
+	-profileProperties org.eclipse.update.install.features=true\
+	-installIU org.openflexo.emfconnector.metamodel.exporter.updatesite.feature.feature.group
+cd ..
+rm -rf director
+rm -rf updatesite-org.openflexo.emfconnector.metamodel.exporter
