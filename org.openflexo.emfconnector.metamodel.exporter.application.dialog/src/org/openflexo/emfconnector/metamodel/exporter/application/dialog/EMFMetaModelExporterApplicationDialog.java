f903f04bba6d50e822e7efdf7be6c19861b2f3ce
Gilles Besançon <gilles.besancon@gmail.com>
2013-11-24 21:37:41 -0600

    Dialog application for exporter.

--- org.openflexo.emfconnector.metamodel.exporter.application.dialog/src/org/openflexo/emfconnector/metamodel/exporter/application/dialog/EMFMetaModelExporterApplicationDialog.java
+++ org.openflexo.emfconnector.metamodel.exporter.application.dialog/src/org/openflexo/emfconnector/metamodel/exporter/application/dialog/EMFMetaModelExporterApplicationDialog.java
@@ -1,6 +1,8 @@
 package org.openflexo.emfconnector.metamodel.exporter.application.dialog;
 
+import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
+import org.openflexo.emfconnector.metamodel.exporter.EMFMetaModelExportWizard;
 
 public class EMFMetaModelExporterApplicationDialog extends AGraphicalApplication {
 
@@ -10,5 +12,8 @@ public class EMFMetaModelExporterApplicationDialog extends AGraphicalApplication
 
 	@Override
 	protected void createShellContent(Shell shell) {
+		EMFMetaModelExportWizard wizard = new EMFMetaModelExportWizard();
+		WizardDialog dialog = new WizardDialog(shell, wizard);
+		dialog.open();
 	}
 }
